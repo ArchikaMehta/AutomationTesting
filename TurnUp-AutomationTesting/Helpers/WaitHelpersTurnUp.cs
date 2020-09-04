aa819a131a21e96cc3d0339eb62c73b824df8372
ArchikaMehta <archika.mehta19@gmail.com>
2020-09-04 21:26:27 +1200

    Introduced explicit wait

---
+++ TurnUp-AutomationTesting/Helpers/WaitHelpersTurnUp.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace TurnUp_AutomationTesting.Helpers
+{
+    class WaitHelpersTurnUp
+    {
+        public static void WaitClickable(IWebDriver driver, String attribute, String value, int seconds)
+        { 
+            if (attribute =="Id")
+            {
+                var wait = new WebDriverWait(driver, new TimeSpan(0, 0, seconds));
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable((By.Id(value))));
+            }
+            else if (attribute == "XPath")
+            {
+                var wait = new WebDriverWait(driver, new TimeSpan(0, 0, seconds));
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath(value)));
+            }
+            else if (attribute == "CssSelector")
+            {
+                var wait = new WebDriverWait(driver, new TimeSpan(0, 0, seconds));
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector(value)));
+            }
+        }
+    }
+}
